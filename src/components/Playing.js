3b52e78a0f29dc1e61f5833090305daec3fffb18
unknown <haulamle@gmail.com>
2022-04-11 20:04:38 +0700

    first commit

---
+++ src/components/Playing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import AudioPlayer from 'react-h5-audio-player';
+import { Songs } from '../Context'
+import 'react-h5-audio-player/lib/styles.css';
+export default function Playing() {
+    const { song, handleSetSong } = React.useContext(Songs)
+    const handleClickNext = () => {
+        handleSetSong(song.id + 1)
+    } 
+    const handleClickPrevious = () => {
+        handleSetSong(song.id - 1)
+    } 
+    return (
+    <div>
+       <AudioPlayer className="player-music" src={song.url} layout="stacked-reverse"
+       showSkipControls={true} showJumpControls={false} 
+       onClickNext={handleClickNext}
+       onClickPrevious={handleClickPrevious}
+       />
+    </div>
+  )
+}
